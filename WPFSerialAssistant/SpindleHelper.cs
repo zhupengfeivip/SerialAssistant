c8989f0134867118c0e43b061a9746816ddf9ae6
zhupengfei <zhupengfeivip@163.com>
2023-04-12 21:55:11 +0800

    f

---
+++ WPFSerialAssistant/SpindleHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSerialAssistant
+{
+    public class SpindleHelper
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        private byte CheckSum(byte[] bytes)
+        {
+            long sumValue = 0;
+            foreach (byte b in bytes)
+                sumValue += b;
+
+            return (byte)(sumValue);
+        }
+
+
+
+
+
+    }
+}
